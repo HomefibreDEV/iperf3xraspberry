a9aca6302dd0faaf1e985e13c0e60a52884bc678
homefibre digital network gmbh <164861701+HomefibreDEV@users.noreply.github.com>
2024-03-27 15:31:53 +0100

    Update server.sh
    write config in  >> /etc/network/interfaces

--- server.sh
+++ server.sh
@@ -40,11 +40,11 @@ echo 'iPerf-Server' > /etc/hostname
 #network configuration
 echo 'configure network'
 echo "" >> /etc/network/interfaces
-echo '# custom static network config for iPerf client' >> /etc/dhcpcd.conf
-echo 'auto eth0' >> /etc/dhcpcd.conf
-echo 'iface eth0 inet static' >> /etc/dhcpcd.conf
-echo '   address 10.10.10.111/24' >> /etc/dhcpcd.conf
-echo '   gateway 10.10.10.254' >> /etc/dhcpcd.conf
+echo '# custom static network config for iPerf client' >> /etc/network/interfaces
+echo 'auto eth0' >> /etc/network/interfaces
+echo 'iface eth0 inet static' >> /etc/network/interfaces
+echo '   address 10.10.10.111/24' >> /etc/network/interfaces
+echo '   gateway 10.10.10.254' >> /etc/network/interfaces
 
 #change motd
 echo "`clear`" > /etc/motd
